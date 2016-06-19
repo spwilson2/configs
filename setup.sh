eaf8a4778bee93cbda011297a02151b531779acb
Sean Wilson <spwilson27@gmail.com>
2016-06-18 20:00:49 -0500

    Update setup script, include vim config

--- setup.sh
+++ setup.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+	SOURCE="$(readlink "$SOURCE")"
+	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 FILES="bashrc     
 gitconfig           
 setup.sh
 dircolors  
 tmux.conf
-"
+vim"
 
 for FILE in $FILES; do
-    cp $FILE ~/.$FILE
+    ln -s "$DIR/$FILE" "$HOME/.$FILE"
 done
+
+# Run vim setup
+bash "$DIR/vim/setup.sh"
