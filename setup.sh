b28aedb381e1b24d4cceac4939da8450dda0cb6c
Sean Wilson <spwilson27@gmail.com>
2016-07-30 20:00:00 -0500

    Finally fixed

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
 	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
@@ -7,14 +6,11 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-cd $DIR
-
 FILES="bashrc gitconfig dircolors tmux.conf"
-
 for FILE in $FILES; do
-    ln -s "$FILE" "$HOME/.$FILE"
+    ln -s "$DIR/$FILE" "$HOME/.$FILE"
 done
 
 # Run vim setup
-git pull && git submodule update --init --recursive
-bash "vim/setup.sh"
+git pull; git submodule update --init --recursive
+bash "$DIR/vim/setup.sh"
